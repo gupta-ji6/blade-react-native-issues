dc13fd245bd47c3cb430a06b877a539fec8d8fd9
Ayush Gupta <ayush.gupta@razorpay.com>
2023-10-31 03:11:36 +0530

    feat: add react-navigation

--- App.tsx
+++ App.tsx
@@ -6,37 +6,16 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
+
 import {BladeProvider} from '@razorpay/blade/components';
 import {paymentTheme} from '@razorpay/blade/tokens';
 
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import Navigation from './Navigation';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <BladeProvider themeTokens={paymentTheme} colorScheme="light">
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-        </ScrollView>
-      </SafeAreaView>
+      <Navigation />
     </BladeProvider>
   );
 }
